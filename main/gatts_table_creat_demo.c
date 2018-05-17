3b64c33fac3c2c8f9be32ad2860a4026fcc05b83
Ryan Holeman <ryan@hackgnar.com>
2018-05-16 22:14:58 -0500

    typo

--- main/gatts_table_creat_demo.c
+++ main/gatts_table_creat_demo.c
@@ -870,7 +870,6 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_
                         flag_state[16] = 'T';
                     }
                     if (strcmp(writeData,"fc920c68b6006169477b") == 0){
-                    "fc920c68b6006169477b"
                         //hidden notify
                         flag_state[17] = 'T';
                     }
