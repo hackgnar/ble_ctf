ab0dff332403bdb8ca10fdd28d66fbdbf00f8a6e
Ryan Holeman <ryan@hackgnar.com>
2018-06-08 10:59:56 -0500

    fixed some of the md5 flag values because they had newline chars in the value

--- main/gatts_table_creat_demo.c
+++ main/gatts_table_creat_demo.c
@@ -843,7 +843,7 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_
                         //simple read
                         flag_state[2] = 'T';
                     }
-                    if (strcmp(writeData,"0096ff9a0051c6f82607") == 0){
+                    if (strcmp(writeData,"5cd56d74049ae40f442e") == 0){
                         //md5 of device name
                         flag_state[3] = 'T';
                     }
@@ -907,7 +907,7 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_
                         //hidden notify
                         flag_state[18] = 'T';
                     }
-                    if (strcmp(writeData,"7b6b57120625e6fcb84a") == 0){
+                    if (strcmp(writeData,"d953bfb9846acc2e15ee") == 0){
                         //final flag
                         flag_state[19] = 'T';
                     }
