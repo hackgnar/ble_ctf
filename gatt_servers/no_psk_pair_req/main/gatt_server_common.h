9449a6f6e5dd177b46ec9626a8547ee698b1cdca
Ryan Holeman <ryan@hackgnar.com>
2019-05-05 21:09:01 +1000

    added a flag for no psk pairing

---
+++ gatt_servers/no_psk_pair_req/main/gatt_server_common.h
@@ -0,0 +1,45 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+/****************************************************************************
+*
+* This file is for a gatt server CTF (capture the flag). 
+*
+****************************************************************************/
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_bt.h"
+#include "driver/gpio.h"
+
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_main.h"
+#include "esp_gatt_common_api.h"
+
+typedef struct {
+    uint8_t                 *prepare_buf;
+    int                     prepare_len;
+} prepare_type_env_t;
+
+static prepare_type_env_t prepare_write_env;
+
+
+//KEEP
+#define GATTS_TABLE_TAG "BLE_CTF"
+
+//KEEP
+#define PREPARE_BUF_MAX_SIZE        1024
+
+void example_prepare_write_event_env(esp_gatt_if_t gatts_if, prepare_type_env_t *prepare_write_env, esp_ble_gatts_cb_param_t *param);
+
+void example_exec_write_event_env(prepare_type_env_t *prepare_write_env, esp_ble_gatts_cb_param_t *param);
